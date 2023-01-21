9b3ec0b2b8bdabb8e844dafdd4b81c146eaff2ad
Anass Salama <asalama@smb.sqli.com>
2023-01-21 18:16:58 +0100

    fix navbar & add cursor folowup

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,26 @@ menuToggle.onclick = function(){
     menuToggle.classList.toggle('open');
     navigation.classList.toggle('open');
 }
+
+function move(){
+    var IE = document.all;
+    
+    var tempX = 0;
+    var tempY = 0;
+
+    var element=document.getElementById("cursor");
+    
+    document.onmousemove=function getMouseXY(abc) {
+        if (IE) { // grab the x-y pos.s if browser is IE
+            tempX = event.clientX;
+            tempY = event.clientY;
+        }
+        else { // grab the x-y pos.s if browser is NS
+            tempX = abc.pageX;
+            tempY = abc.pageY;
+        }
+        
+        element.style.marginLeft = tempX + "px";
+        element.style.marginTop = tempY + "px";
+    }
+}
