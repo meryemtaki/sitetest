07ae639a36382c1a4e34abf39c80c61982b30f97
Meryem TAKI <meryemtaki968@gmail.com>
2023-01-23 23:29:27 +0100

    page sector

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,25 +7,11 @@ menuToggle.onclick = function(){
     html.classList.toggle('overflow')
 }
 
-function move(){
-    var IE = document.all;
-    
-    var tempX = 0;
-    var tempY = 0;
 
-    var element=document.getElementById("cursor");
-    
-    document.onmousemove=function getMouseXY(abc) {
-        if (IE) { // grab the x-y pos.s if browser is IE
-            tempX = event.clientX;
-            tempY = event.clientY;
-        }
-        else { // grab the x-y pos.s if browser is NS
-            tempX = abc.pageX;
-            tempY = abc.pageY;
-        }
-        
-        element.style.marginLeft = tempX + "px";
-        element.style.marginTop = tempY + "px";
-    }
-}
+
+$('.slide').slick({
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+});
+          
