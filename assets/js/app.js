c4e2f89ec73b97fef846827d688327019a8d3dd0
Meryem TAKI <meryemtaki968@gmail.com>
2023-01-24 22:56:01 +0100

    add 5 page respo

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,35 @@ menuToggle.onclick = function(){
     html.classList.toggle('overflow')
 }
 
+let submenu = document.querySelector('.arrow');
+let menu_submenu = document.querySelector('.submenu');
+submenu.onclick = function(){
+    submenu.classList.toggle('show');
+    menu_submenu.classList.toggle('show');
+}
+
+function move(){
+  var IE = document.all;
+  
+  var tempX = 0;
+  var tempY = 0;
+
+  var element=document.getElementById("cursor");
+  
+  document.onmousemove=function getMouseXY(abc) {
+      if (IE) { // grab the x-y pos.s if browser is IE
+          tempX = event.clientX;
+          tempY = event.clientY;
+      }
+      else { // grab the x-y pos.s if browser is NS
+          tempX = abc.pageX;
+          tempY = abc.pageY;
+      }
+      
+      element.style.marginLeft = tempX + "px";
+      element.style.marginTop = tempY + "px";
+  }
+}
 
 
 $('.slide').slick({
@@ -23,4 +52,6 @@ $('.slide').slick({
         },
     ],
 });
-          
+
+
+
